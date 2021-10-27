c5c6633400b131aa365ad6b24bbbc10f6dc78b88
Anna <acdodd17@gmail.com>
2021-10-27 12:26:56 -0400

    form usability

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,12 @@ var taskFormHandler = function(event) {
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
+
+    // check if input values are empty strings
+    if (!taskNameInput || !taskTypeInput) {
+        alert("You need to fill out the task form!");
+        return false;
+    }
     // package up data as an object
     var taskDataObj = {
         name: taskNameInput,
@@ -13,8 +19,12 @@ var taskFormHandler = function(event) {
     };
 
     // send it as an argument to createTaskEl
-    createTaskEl(taskDataObj);
+    createTaskE1(taskDataObj);
+
+    formEl.reset();
 };
+
+
 
 var createTaskE1 = function(taskDataObj) {
     // create list item
